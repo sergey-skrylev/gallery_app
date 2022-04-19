cefcce8430ce5406f3b7db50d3a39a0fc86bb184
Sergey <sergey.s1991@yandex.ru>
2022-04-19 15:14:32 +0300

    install packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import React from 'react';
+
+const App = () => {
   return (
-    <div className="App">
-      Works
+    <div>
+      works
     </div>
   );
-}
+};
 
 export default App;
