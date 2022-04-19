47c815f7bf5fc913a114702d440a6b6045265833
Sergey <sergey.s1991@yandex.ru>
2022-04-19 15:24:59 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Gallery from './components/Gallery'
+import AboutMe from './components/AboutMe'
+import PhotoItem from './components/AboutMe'
 
 const App = () => {
   return (
-    <div>
-      works
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Gallery />} />
+          <Route path='/aboutme' element={<AboutMe />} />
+          <Route path="photos/:id" element={<PhotoItem />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
---
+++ src/components/PhotoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoItem = () => {
+  return (
+    <div>
+      Uniq Photo
+    </div>
+  );
+};
+
+export default PhotoItem;
