8a70f19709ad577f295cf2a216b2244e355b33cf
Sergey <sergey.s1991@yandex.ru>
2022-04-19 16:37:47 +0300

    add fetching photos to redux store

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card } from 'react-bootstrap';
-
+import { useSelector, useDispatch } from 'react-redux'
+import axios from 'axios';
+import { setPhotos } from '../redux/actions/actions'
 
 const Gallery = () => {
+
+  const photos = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const fetchPhotos = async () => {
+    const response = await axios
+      .get('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=24')
+      .catch((err) => {
+        console.log('error', err);
+      })
+    dispatch(setPhotos(response.data));
+  }
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+  console.log(photos);
+
   return (
     <div>
        <Card style={{ width: '18rem' }}>
